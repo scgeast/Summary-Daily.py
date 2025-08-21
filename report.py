afb595ca5599a5ecc6b1cfd9f1b1563844be2e6f
scgeast <scgeast@gmail.com>
2025-08-21 10:37:22 +0700

    Update report.py

--- report.py
+++ report.py
@@ -35,7 +35,7 @@ else:
 
     # Apply filters
     if len(Delivery_Date_range) == 2:
-        df = df[(df["Delivery_Date"] >= pd.to_Delivery_date time(Delivery_Date_range[0])) & (df["Delivery_Date"] <= pd.to_datetime(Delivery_Date_range[1]))]
+     df = df[(df["Delivery_Date"] >= pd.to_datetime(Delivery_Date_range)) & (df["Delivery_Date"] <= pd.to_datetime(Delivery_Date_range))]
     if REGION:
         df = df[df["REGION"].isin(REGION)]
     if plant_name:
